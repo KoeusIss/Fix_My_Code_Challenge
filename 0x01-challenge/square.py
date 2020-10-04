eb845f5f119881ab095adef2f08f8610e50376c1
KoeusIss <sebri.issam@gmail.com>
2020-10-04 19:23:42 +0100

    fix the naming convention

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Square module"""
+""" Square module """
 
 
 class square():
-    """Square class"""
+    """ Square class """
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
-        """  """
+        """ Initializer """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,16 +16,17 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def perimiter_of_my_square(self):
         """ Perimiter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """  """
+        """ Square string """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
+    """ Import """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.perimiter_of_my_square())
