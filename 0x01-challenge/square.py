c8b1a1cec8d4c2b67511098cabb5051ded6f7d25
KoeusIss <sebri.issam@gmail.com>
2020-10-04 18:53:09 +0100

    try with the original version of the file

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""Square module"""
-
 
 class square():
-    """ Square class """
+    
     width = 0
     height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """ Initializer """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Perimiter of square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ Object to rtString """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
